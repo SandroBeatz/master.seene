d155e0b48c0b290060180a9c9f672121883f80f8
Aleksandr <35688979+SandroBeatz@users.noreply.github.com>
2026-09-20 22:05:57 +0600

    feat(mobile): добавить Overview на главную (#64)

--- src/pages/home/ui-mobile/HomeMobilePage.vue
+++ src/pages/home/ui-mobile/HomeMobilePage.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
 import { IonContent, IonPage } from '@ionic/vue'
-import { HomeHeaderMobile } from '@widgets/home/index.mobile'
+import { HomeHeaderMobile, HomeOverviewMobile } from '@widgets/home/index.mobile'
 </script>
 
 <template>
   <ion-page>
     <home-header-mobile />
-    <ion-content :fullscreen="true" />
+    <ion-content :fullscreen="true">
+      <main class="home-content">
+        <home-overview-mobile />
+      </main>
+    </ion-content>
   </ion-page>
 </template>
+
+<style scoped>
+.home-content {
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+  padding: 12px 14px calc(14px + var(--safe-area-bottom, 0px));
+}
+</style>
--- src/widgets/home/index.mobile.ts
+++ src/widgets/home/index.mobile.ts
@@ -1,1 +1,2 @@
 export { default as HomeHeaderMobile } from './ui-mobile/HomeHeaderMobile.vue'
+export { default as HomeOverviewMobile } from './ui-mobile/HomeOverviewMobile.vue'
